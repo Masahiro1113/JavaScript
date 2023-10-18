9c49fdae0e4aca271b334a0ea639125dca048ada
Masahiro1113 <masahiro1113mm12@gmail.com>
2023-10-18 17:50:50 +0900

    up

--- W02/vec3.js
+++ W02/vec3.js
@@ -4,4 +4,18 @@ class Vec3 {
       this.y = y;
       this.z = z;
     }
+
+    min(){
+        return Math.min(this.x, this.y, this.z);
+    }
+
+    mid(){
+        const arr = [this.x, this.y, this.z];
+        arr.sort((a, b) => a - b);
+        return arr[1];
+    }
+
+    max(){
+        return Math.max(this.x, this.y, this.z);
+    }
 }
